0522983e9e4571d4b109cf47b5ee02f2de8fa71d
Yukio Ejiri <ejiri@camobile.com>
2018-08-14 17:12:48 +0900

    Make project file from the beginning.

---
+++ GPUImageFramework/GPUImageFramework.h
@@ -0,0 +1,19 @@
+//
+//  GPUImageFramework.h
+//  GPUImageFramework
+//
+//  Created by Yukio Ejiri on 2018/08/14.
+//  Copyright © 2018年 C.A. Mobile, LTD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GPUImageFramework.
+FOUNDATION_EXPORT double GPUImageFrameworkVersionNumber;
+
+//! Project version string for GPUImageFramework.
+FOUNDATION_EXPORT const unsigned char GPUImageFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GPUImageFramework/PublicHeader.h>
+
+
